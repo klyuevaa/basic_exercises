c26168b9951fbaa5c4800eadc8599cdae6f9e817
klyuevaa <python.klyuev@gmail.com>
2021-12-03 13:43:18 +0300

    done

--- string_challenges.py
+++ string_challenges.py
@@ -1,28 +1,40 @@
 # Вывести последнюю букву в слове
 word = 'Архангельск'
-# ???
+print(word[-1])
 
 
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-# ???
+print(word.count('а'))
 
 
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
-# ???
-
+vowels = set('аеиоуэюя')
+#print(sum(letter in vowels for letter in word.lower()))
+count=0
+for letter in word.lower():
+    if letter in vowels:
+        count+=1
+print(count)
 
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
-# ???
+print(len(sentence.split(' ')))
 
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
+for word in sentence.split(' '):
+    print(word[0])
 
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+count = 0
+len_word = 0
+for word in sentence.split(' '):
+    len_word += len(word)
+    count += 1
+print('всреднем', len_word / count, 'букв')
+
